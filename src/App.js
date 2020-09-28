80b98f1ad8b23b0a8e5c3a3138fd878eab6b2ae1
ChloeSilent <hloesilent@gmail.com>
2020-09-28 15:55:44 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import audio0 from "./sounds/1.ogg";
+import audio1 from "./sounds/2.ogg";
+import audio2 from "./sounds/3.ogg";
+import audio3 from "./sounds/4.mp3";
+
+const audios = {
+    "top-left": audio0,
+    "top-right": audio1,
+    "bottom-left": audio2,
+    "bottom-right": audio3
+};
 
 class App extends React.Component {
     state = {
@@ -7,19 +19,20 @@ class App extends React.Component {
         sequence: [],
         sequenceToGuess: [],
         active: null,
-        status: 0
+        round: 0,
+        status: ""
     }
 
     getRandomPanel = () => {
         const panels = ["top-left", "top-right", "bottom-left", "bottom-right"];
         const randomPanel = panels[parseInt(Math.random() * panels.length, 10)];
-        console.log("getRandomPanel", typeof randomPanel, randomPanel);
         return randomPanel
     };
 
     flash = (panel) => {
-        console.log("flash", panel)
         return new Promise((resolve) => {
+            // const sound = new Audio(audios[panel])
+            // sound.play();
             this.setState({
                 active: panel
             })
@@ -29,7 +42,7 @@ class App extends React.Component {
                 })
                 setTimeout(() => {
                     resolve();
-                }, 250);
+                }, 500);
             }, 1000);
         });
     };
@@ -42,28 +55,22 @@ class App extends React.Component {
                 sequenceToGuess: [...copySequenceToGuess]
             }, () => {
                 if (expectedPanel === panel) {
-                    // console.log("3 expectedPanel === panel", expectedPanel === panel);
-                    // console.log("3 copySequenceToGuess", copySequenceToGuess)
-                    // console.log("3 this.state.sequenceToGuess.length", this.state.sequenceToGuess.length)
-                    // console.log("3 this.state.sequenceToGuess", this.state.sequenceToGuess)
                     if (this.state.sequenceToGuess.length === 0) {
                         console.log("start a new round");
                         const update = [this.getRandomPanel()]
                         this.setState({
                             sequence: this.state.sequence.concat(update),
                             sequenceToGuess: this.state.sequence.concat(update),
-                            status: this.state.status + 1
+                            round: this.state.round + 1
                         })
                         this.startFlashing();
-                    } else {
-                        console.log("hohoho")
                     }
                 } else {
-                    alert("game over");
                     this.setState({
                         sequence: [],
                         sequenceToGuess: [],
-                        status: 0
+                        round: 0,
+                        status: "game over"
                     })
                 }
             })
@@ -72,22 +79,46 @@ class App extends React.Component {
     };
 
     startFlashing = async () => {
-        this.setState({
-            canClick: false
-        })
+        if (this.state.round >= 21) {
+            this.setState({
+                sequence: [],
+                sequenceToGuess: [],
+                round: 0,
+                status: "You win!"
+            })
+        } else {
+            this.setState({
+                canClick: false
+            })
 
-        for (const panel of this.state.sequence) {
-            await this.flash(panel);
+            for (const panel of this.state.sequence) {
+                await this.flash(panel);
+            }
+            this.setState({
+                canClick: true,
+            })
         }
-        this.setState({
-            canClick: true,
-            status: 0
-        })
     };
+
+    startGame = async () => {
+        setTimeout(async () => {
+            const item = this.getRandomPanel();
+            this.setState({
+                canClick: false,
+                sequence: [item],
+                sequenceToGuess: [item],
+                active: null,
+                round: 0,
+                status: ""
+            })
+            for (const panel of this.state.sequence) {
+                await this.flash(panel);
+            }
+        }, 300)
+    }
 
     componentDidMount() {
         const item = this.getRandomPanel();
-        console.log("item", item);
         this.setState({
             sequence: [item],
             sequenceToGuess: [item]
@@ -97,39 +128,48 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <div className="simonPanel">
-                    <div className="row">
-                        <div onClick={() => {
-                            this.panelClicked("top-left")
-                        }}
-                             className={`top-left panel ${this.state.active === "top-left" ? "active" : ""}`}>
+                <div className="title">
+                    <h1>Simon</h1>
+                    <button className="play-button" onClick={this.startFlashing}>Start</button>
+                </div>
+                <div className="game">
+                    <div className="simonPanel">
+                        <div className="row">
+                            <div onClick={() => {
+                                this.panelClicked("top-left")
+                            }}
+                                 className={`top-left panel ${this.state.active === "top-left" ? "active" : ""}`}>
+                            </div>
+                            <div onClick={() => {
+                                this.panelClicked("top-right")
+                            }}
+                                 className={`top-right panel ${this.state.active === "top-right" ? "active" : ""}`}>
+                            </div>
                         </div>
-                        <div onClick={() => {
-                            this.panelClicked("top-right")
-                        }}
-                             className={`top-right panel ${this.state.active === "top-right" ? "active" : ""}`}>
+                        <div className="row">
+                            <div onClick={() => {
+                                this.panelClicked("bottom-left")
+                            }}
+                                 className={`bottom-left panel ${this.state.active === "bottom-left" ? "active" : ""}`}>
+                            </div>
+                            <div onClick={() => {
+                                this.panelClicked("bottom-right")
+                            }}
+                                 className={`bottom-right panel ${this.state.active === "bottom-right" ? "active" : ""}`}>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div onClick={() => {
-                            this.panelClicked("bottom-left")
-                        }}
-                             className={`bottom-left panel ${this.state.active === "bottom-left" ? "active" : ""}`}>
-                        </div>
-                        <div onClick={() => {
-                            this.panelClicked("bottom-right")
-                        }}
-                             className={`bottom-right panel ${this.state.active === "bottom-right" ? "active" : ""}`}>
-                        </div>
+                    <div className="additionalData">
+                        <div>Round : {this.state.round}</div>
                     </div>
                 </div>
-                <div className="additionalData">
-                    <div>sequence: {this.state.sequence}</div>
-                    <div>sequenceToGuess: {this.state.sequenceToGuess}</div>
-                    <div>status: {this.state.status}</div>
-                    <button onClick={this.startFlashing}>Start</button>
+                <div
+                    className={`modal ${this.state.status === "You win!" || this.state.status === "game over" ? "shown" : ""}`}>
+                    <div className="message">
+                        <p>{this.state.status}</p>
+                        <button className="play-button" onClick={this.startGame}>Try again</button>
+                    </div>
                 </div>
-
             </div>
         );
     }
